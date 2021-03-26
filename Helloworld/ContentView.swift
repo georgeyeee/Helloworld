a0e61c9d16a3711d5d1413a06fb427e9e0f2a82b
georgeyeee <liveyei@gmail.com>
2021-03-26 12:24:20 +0800

    Initial Commit

---
+++ Helloworld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Helloworld
+//
+//  Created by Mac on 2021/3/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Helloworld/HelloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloworldApp.swift
+//  Helloworld
+//
+//  Created by Mac on 2021/3/26.
+//
+
+import SwiftUI
+
+@main
+struct HelloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
